80afd755baf41bafd9ee04d49729484f421e6316
joker420-cool <77294636+joker420-cool@users.noreply.github.com>
2021-01-11 23:49:06 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5ffc8edc6d6deb001f91af74",
+				"5ffc8edc6d6deb001f91af76"
+			]
+		},
+		"5ffc8edc6d6deb001f91af74": {
+			"id": "5ffc8edc6d6deb001f91af74",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5ffc8edc6d6deb001f91af76": {
+			"id": "5ffc8edc6d6deb001f91af76",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
---
+++ src/pages/index.js
@@ -0,0 +1,77 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Button, Text, Icon, Box } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { Override, StackItem, Stack, Section } from "@quarkly/components";
+import { MdArrowDownward } from "react-icons/md";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Button
+			position="fixed"
+			top="20px"
+			left="20px"
+			z-index="2"
+			href="/"
+		>
+			MENU
+		</Button>
+		<Section
+			background="linear-gradient(0deg,rgba(4, 8, 12, 0.6) 0%,rgba(4, 8, 12, 0.6) 100%),--color-darkL2 url(https://images.unsplash.com/photo-1510125594188-5afc74c8cc43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80) center/cover"
+			padding="64px 0"
+			sm-padding="40px 0"
+			color="#1ab631"
+			font="normal 300 16px/1.5 --fontFamily-sans"
+			hover-color="#2be22b"
+			hover-text-transform="uppercase"
+		>
+			<Text as="h1" font="normal 900 72px/1.2 --fontFamily-sans" md-font="--headline2" margin="10px 0">
+				IT IS NOT ABOUT LIVING FOR EVER.{" "}
+				<br />
+				{" "}THE TRICK IS LIVING YOUR SELF FOR EVER.
+			</Text>
+			<Stack>
+				<StackItem width="75%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text color="--lightD2" letter-spacing="1px" text-transform="uppercase" margin="0">
+						Excellence in everything
+					</Text>
+				</StackItem>
+			</Stack>
+			<Box text-align="center" margin="96px 0 0 0">
+				<Text margin="8px 0" text-transform="uppercase">
+					Who we are
+				</Text>
+				<Icon category="md" margin="0 auto" icon={MdArrowDownward} />
+			</Box>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
